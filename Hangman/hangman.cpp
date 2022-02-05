d45cc515de7c39c144517ee043e22203cc11fe4e
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-02-05 22:37:56 +0530

    still a bug to fix

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void printmsg(string message, bool printTop= true, bool printBottom= true){
+    if(printTop)
+    {
+        cout <<  "+---------------------------------------+" << endl;
+        cout << "|";
+    }
+
+    bool front = true;
+    for(int i = message.length(); i < 41; i++)
+    {
+        if(front)
+        {
+            message = " " + message;
+        }
+        else
+        {
+            message = message + " ";
+        }
+        front = !front;
+    }
+
+    if(printBottom)
+    {
+        cout << "|";
+        cout << "+---------------------------------------+";
+    }
+}
+
 int main()
 {
-    
+    printmsg("test");
 }
