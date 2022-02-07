f15ea177560acd1afbad02394872c22b06caedc6
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-02-07 15:28:54 +0530

    fixed local compiler issue

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -7,9 +7,12 @@ void printmsg(string message, bool printTop= true, bool printBottom= true){
         cout <<  "+---------------------------------------+" << endl;
         cout << "|";
     }
+    else{
+        cout << "|";
+    }
 
     bool front = true;
-    for(int i = message.length(); i < 41; i++)
+    for(int i = message.length(); i < 39; i++)
     {
         if(front)
         {
@@ -21,15 +24,36 @@ void printmsg(string message, bool printTop= true, bool printBottom= true){
         }
         front = !front;
     }
+    cout << message;
 
     if(printBottom)
     {
-        cout << "|";
-        cout << "+---------------------------------------+";
+        cout << "|" << endl;
+        cout << "+---------------------------------------+" << endl;
+    }
+    else{
+        cout << "|" << endl;
+    }
+}
+
+void drawHangman(int count = 0)
+{
+    if(count >= 1){
+        printmsg("|", false, false);
+    }
+    if(count >= 2){
+        printmsg("|", false, false);
+    }
+    if(count >=3){
+        printmsg("O", false, false);
+    }
+    if(count ==4){
+        printmsg("|", false, false);
     }
 }
 
 int main()
 {
-    printmsg("test");
+    printmsg("Hangman");
+    drawHangman(10);
 }
