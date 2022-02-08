16bad758c7cd214524d10e2f11b7da3d59f6af58
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-02-08 11:30:30 +0530

    working on win checker function

--- Hangman/hangman.cpp
+++ Hangman/hangman.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void printmsg(string message, bool printTop= true, bool printBottom= true){
+
+// printMsg function prints messeage with or without border
+// Will be used multiple times throughout the code
+void printMsg(string message, bool printTop= true, bool printBottom= true){
     if(printTop)
     {
         cout <<  "+---------------------------------------+" << endl;
@@ -36,42 +39,81 @@ void printmsg(string message, bool printTop= true, bool printBottom= true){
     }
 }
 
+// drawHangman will redraw hangman, every guess will slowly complete it
 void drawHangman(int count = 0)
 {
     if(count >= 1){
-        printmsg("|", false, false);
+        printMsg("|", false, false);
     }
     if(count >= 2){
-        printmsg("|", false, false);
+        printMsg("|", false, false);
     }
     if(count >=3){
-        printmsg("O", false, false);
+        printMsg("O", false, false);
     }
     if(count ==4){
-        printmsg("|", false, false);
+        printMsg("|", false, false);
     }
     if(count ==5){
-        printmsg("|\\", false, false);
+        printMsg("|\\", false, false);
     }
     if(count >= 6){
-        printmsg("/|\\", false, false);
+        printMsg("/|\\", false, false);
     }
     if(count == 7){
-        printmsg(" \\", false, false);
+        printMsg(" \\", false, false);
     }
     if(count >= 8){
-        printmsg("/ \\", false, false);
+        printMsg("/ \\", false, false);
     }
     if(count >=9){
-        printmsg("+--------+", false, false);
+        printMsg("+--------+", false, false);
     }
     if(count ==10){
-        printmsg("+         +", false, false);
+        printMsg("|        |", false, false);
     }
 }
 
+// printLetters used to print all letters
+// It will also redraw the letters and remove them with each guess
+void printLetters(string input, char from, char to, bool top, bool bottom)
+{
+    string s;
+    for(char i = from; i <= to; i++)
+    {
+        if(input.find(i) == string::npos)
+        {
+            s+=i;
+            s+=" ";
+        }
+        else
+        {
+            s+=" ";
+        }
+    }
+    printMsg(s, top, bottom);
+}
+
+// function checks if letter guessed by user is in the word or not
+bool check(string word, char guess)
+{
+    bool won = true;
+    string s;
+    for(int i  = 0; i < word.length(); i++)
+    {
+        if()
+    }
+
+}
+
+// main fuction
 int main()
 {
-    printmsg("Hangman");
+    printMsg("Hangman");
     drawHangman(10);
+    printLetters("", 'A', 'M', true, false);
+    printLetters("", 'N', 'Z', false, true);
+
+    getchar();
+    return 0;
 }
